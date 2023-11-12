e41d4c100ec7d2aae40ac7bc7d92743c4c3581cc
michaelpierce <michael.todd.pierce@gmail.com>
2023-11-11 21:01:23 -0400

    Initial Employee Overview.razor

---
+++ BethanysPieShop.WebFrontEnd/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BethanysPieShop.WebFrontEnd.Models;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace BethanysPieShop.WebFrontEnd.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+            base.OnInitialized();
+        }
+    }
+}
+
